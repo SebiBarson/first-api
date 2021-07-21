abd09a81eed5e1a419bfd525ca04e87622c5344d
SebiBarson <s.bodea@yahoo.com>
2021-07-21 11:48:00 +0300

    Removed unnecessary using's

--- Tweetbook/Startup.cs
+++ Tweetbook/Startup.cs
@@ -1,19 +1,11 @@
+
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.Swagger;
-using Swashbuckle.Swagger;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Tweetbook.Data;
-using Tweetbook.Options;
 
 namespace Tweetbook
 {
