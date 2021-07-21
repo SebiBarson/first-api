5a1405fcb727702d67043be25bce9883357ab2cb
SebiBarson <s.bodea@yahoo.com>
2021-07-21 12:11:00 +0300

    Applied code cleanup

--- Tweetbook/Controllers/TestController.cs
+++ Tweetbook/Controllers/TestController.cs
@@ -1,17 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Tweetbook.Controllers
 {
-    public class TestController: Controller
+    public class TestController : Controller
     {
-        /// <summary>
-        /// Ciueva
-        /// </summary>
-        /// <returns></returns>
         [HttpGet("api/user")]
         public IActionResult Get()
         {
--- Tweetbook/Startup.cs
+++ Tweetbook/Startup.cs
@@ -56,7 +56,7 @@ namespace Tweetbook
             {
                 endpoints.MapControllers();
             });
-            
+
         }
     }
 }
