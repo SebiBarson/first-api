f1321f75e40ba93bbd4dcbba2eff195d7e313d27
SebiBarson <s.bodea@yahoo.com>
2021-07-21 17:11:04 +0300

    Implemented dependency injection
    https://www.youtube.com/watch?v=ESdvXlrG9zQ&list=PLUOequmGnXxOgmSDWU7Tl6iQTsOtyjtwU&index=3&ab_channel=NickChapsas

---
+++ Tweetbook/Installers/SwaggerInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Tweetbook.Installers
+{
+    public class SwaggerInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo { Title = "Title", Version = "v1" });
+            });
+            services.AddControllers();
+        }
+    }
+}
