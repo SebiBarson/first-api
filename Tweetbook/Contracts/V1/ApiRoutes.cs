3551e7d4155d28c63efdbf39c3d17a498d5e4bb3
SebiBarson <s.bodea@yahoo.com>
2021-07-21 13:40:24 +0300

    Iimplemented versioning

---
+++ Tweetbook/Contracts/V1/ApiRoutes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tweetbook.Contracts.V1
+{
+    public static class ApiRoutes
+    {
+        public const string Route = "api";
+        public const string Version = "v1";
+        public const string Base = Route + "/" + Version;
+        public static class Posts
+        {
+            public const string GetAll = Base + "/posts";
+        }
+    }
+}
