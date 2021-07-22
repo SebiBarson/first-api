a8fa75201fd986af01355a9cf817ba414d71a281
SebiBarson <s.bodea@yahoo.com>
2021-07-22 11:11:13 +0300

    Updated variable naming

--- Tweetbook/Installers/InstallerExtensions.cs
+++ Tweetbook/Installers/InstallerExtensions.cs
@@ -9,8 +9,8 @@ namespace Tweetbook.Installers
     {
         public static void InstallServicesInAssembly(this IServiceCollection services, IConfiguration configuration)
         {
-            var installers = typeof(Startup).Assembly.ExportedTypes.Where(x =>
-                typeof(IInstaller).IsAssignableFrom(x) && !x.IsAbstract).Select(Activator.CreateInstance).Cast<IInstaller>().ToList();
+            var installers = typeof(Startup).Assembly.ExportedTypes.Where(installerType =>
+                typeof(IInstaller).IsAssignableFrom(installerType) && !installerType.IsAbstract).Select(Activator.CreateInstance).Cast<IInstaller>().ToList();
 
             installers.ForEach(installer => installer.InstallServices(services, configuration));
         }
