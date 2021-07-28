16523742c3ee15395e9710b50b60ffa31d4d6bd4
SebiBarson <s.bodea@yahoo.com>
2021-07-28 15:36:34 +0300

    Added token properties

--- Tweetbook/Options/JwtSettings.cs
+++ Tweetbook/Options/JwtSettings.cs
@@ -1,7 +1,10 @@
-﻿namespace Tweetbook.Options
+﻿using System;
+
+namespace Tweetbook.Options
 {
     public class JwtSettings
     {
+        public TimeSpan TokenLifetime { get; set; }
         public string Secret { get; set; }
     }
 }
